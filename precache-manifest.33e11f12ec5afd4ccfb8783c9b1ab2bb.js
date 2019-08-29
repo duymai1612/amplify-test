b83260d24f101806b01b9e39b06c7cd6ecb97768
Duy Mai <duymai1612@gmail.com>
2019-08-29 22:29:53 +0700

    Updates

---
+++ precache-manifest.33e11f12ec5afd4ccfb8783c9b1ab2bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5ba34ce74e2ddc98c850450538f912",
+    "url": "/amplify-test/index.html"
+  },
+  {
+    "revision": "1d4058cde53cf086430b",
+    "url": "/amplify-test/static/css/2.eee0f4a3.chunk.css"
+  },
+  {
+    "revision": "d47fab77a82c80239a9e",
+    "url": "/amplify-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1d4058cde53cf086430b",
+    "url": "/amplify-test/static/js/2.e67b1449.chunk.js"
+  },
+  {
+    "revision": "d47fab77a82c80239a9e",
+    "url": "/amplify-test/static/js/main.99d67bb4.chunk.js"
+  },
+  {
+    "revision": "0dcc417932de3e5b18b7",
+    "url": "/amplify-test/static/js/runtime~main.4b75930c.js"
+  }
+]);
